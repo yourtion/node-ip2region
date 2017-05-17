39fe0ded501ca0d8cccb968e79467a8a6e62e6bf
YourtionGuo <yourtion@gmail.com>
2017-05-17 15:13:54 +0800

    add inMemoryBinarySearch

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 
 const IP2Region = require('../lib');
-const query = new IP2Region();
+const query = new IP2Region({ inMemory: true });
 
 test('btreeSearchSync - Found', t => {
   const res = query.btreeSearchSync('120.24.78.68');
@@ -43,5 +43,26 @@ test('binarySearchSync Parse - Found', t => {
 
 test('binarySearchSync Parse - Not Found', t => {
   const res = query.binarySearchSync('10.10.10.10', true);
+  t.deepEqual(res, { id: 0, country: '未分配或者内网IP', region: '0', province: '0', city: '0', isp: '0' });
+});
+
+
+test('inMemoryBinarySearch - Found', t => {
+  const res = query.inMemoryBinarySearch('120.24.78.68');
+  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+});
+
+test('inMemoryBinarySearch - Not Found', t => {
+  const res = query.inMemoryBinarySearch('10.10.10.10');
+  t.deepEqual(res, { city: 0, region: '未分配或者内网IP|0|0|0|0' });
+});
+
+test('inMemoryBinarySearch Parse - Found', t => {
+  const res = query.inMemoryBinarySearch('120.24.78.68', true);
+  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+});
+
+test('inMemoryBinarySearch Parse - Not Found', t => {
+  const res = query.inMemoryBinarySearch('10.10.10.10', true);
   t.deepEqual(res, { id: 0, country: '未分配或者内网IP', region: '0', province: '0', city: '0', isp: '0' });
 });
