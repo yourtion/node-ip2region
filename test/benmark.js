bdbfca9c00818382b88105014688ff9c33c96081
YourtionGuo <yourtion@gmail.com>
2017-06-28 15:48:42 +0800

    update and lint

--- test/benmark.js
+++ test/benmark.js
@@ -11,26 +11,26 @@ const ip = '191.15.147.55';
 const local = '127.0.0.1';
 
 suite
-.add('inMemoryBinarySearch', function () {
-  query.inMemoryBinarySearch(ip);
-  query.inMemoryBinarySearch(local);
-})
-.add('inMemoryBtreeSearch', function () {
-  query.inMemoryBtreeSearch(ip);
-  query.inMemoryBtreeSearch(local);
-})
-.add('binarySearchSync', function () {
-  query2.binarySearchSync(ip);
-  query2.binarySearchSync(local);
-})
-.add('btreeSearchSync', function () {
-  query2.btreeSearchSync(ip);
-  query2.btreeSearchSync(local);
-})
-.on('cycle', function (event) {
-  console.log(String(event.target));
-})
-.on('complete', function () {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-.run({ 'async': true });
+  .add('inMemoryBinarySearch', function () {
+    query.inMemoryBinarySearch(ip);
+    query.inMemoryBinarySearch(local);
+  })
+  .add('inMemoryBtreeSearch', function () {
+    query.inMemoryBtreeSearch(ip);
+    query.inMemoryBtreeSearch(local);
+  })
+  .add('binarySearchSync', function () {
+    query2.binarySearchSync(ip);
+    query2.binarySearchSync(local);
+  })
+  .add('btreeSearchSync', function () {
+    query2.btreeSearchSync(ip);
+    query2.btreeSearchSync(local);
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({ 'async': true });
