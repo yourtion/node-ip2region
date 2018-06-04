a7ad330ca64187018a519598913631b8c803f00d
YourtionGuo <yourtion@gmail.com>
2018-06-04 16:08:46 +0800

    test: update test

--- test/index.js
+++ test/index.js
@@ -8,100 +8,100 @@ const query2 = new IP2Region();
 
 test('btreeSearchSync - Found', t => {
   const res = query.btreeSearch('120.24.78.68');
-  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+  t.deepEqual(res, { city: 0, region: '中国|0|广东|深圳|阿里云' });
 });
 
 test('btreeSearchSync - Not Found', t => {
   const res = query.btreeSearch('10.10.10.10');
-  t.deepEqual(res, { city: 0, region: '内网IP|0|0|内网IP|内网IP' });
+  t.deepEqual(res, { city: 0, region: '0|0|0|内网IP|内网IP' });
 });
 
 test('btreeSearchSync Parse - Found', t => {
   const res = query.btreeSearch('120.24.78.68', true);
-  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+  t.deepEqual(res, { id: 0, country: '中国', region: '0', province: '广东', city: '深圳', isp: '阿里云' });
 });
 
 test('btreeSearchSync Parse - Not Found', t => {
   const res = query.btreeSearch('10.10.10.10', true);
-  t.deepEqual(res, { id: 0, country: '内网IP', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
+  t.deepEqual(res, { id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 });
 
 
 test('binarySearchSync - Found', t => {
   const res = query.binarySearch('120.24.78.68');
-  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+  t.deepEqual(res, { city: 0, region: '中国|0|广东|深圳|阿里云' });
 });
 
 test('binarySearchSync - Not Found', t => {
   const res = query.binarySearch('10.10.10.10');
-  t.deepEqual(res, { city: 0, region: '内网IP|0|0|内网IP|内网IP' });
+  t.deepEqual(res, { city: 0, region: '0|0|0|内网IP|内网IP' });
 });
 
 test('binarySearchSync Parse - Found', t => {
   const res = query.binarySearch('120.24.78.68', true);
-  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+  t.deepEqual(res, { id: 0, country: '中国', region: '0', province: '广东', city: '深圳', isp: '阿里云' });
 });
 
 test('binarySearchSync Parse - Not Found', t => {
   const res = query.binarySearch('10.10.10.10', true);
-  t.deepEqual(res, { id: 0, country: '内网IP', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
+  t.deepEqual(res, { id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 });
 
 
 test('inMemoryBinarySearch - Found', t => {
   const res = query2.binarySearch('120.24.78.68');
-  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+  t.deepEqual(res, { city: 0, region: '中国|0|广东|深圳|阿里云' });
 });
 
 test('inMemoryBinarySearch - Not Found', t => {
   const res = query2.binarySearch('10.10.10.10');
-  t.deepEqual(res, { city: 0, region: '内网IP|0|0|内网IP|内网IP' });
+  t.deepEqual(res, { city: 0, region: '0|0|0|内网IP|内网IP' });
 });
 
 test('inMemoryBinarySearch Parse - Found', t => {
   const res = query2.binarySearch('120.24.78.68', true);
-  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+  t.deepEqual(res, { id: 0, country: '中国', region: '0', province: '广东', city: '深圳', isp: '阿里云' });
 });
 
 test('inMemoryBinarySearch Parse - Not Found', t => {
   const res = query2.binarySearch('10.10.10.10', true);
-  t.deepEqual(res, { id: 0, country: '内网IP', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
+  t.deepEqual(res, { id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 });
 
 
 test('inMemoryBtreeSearch - Found', t => {
   const res = query2.btreeSearch('120.24.78.68');
-  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+  t.deepEqual(res, { city: 0, region: '中国|0|广东|深圳|阿里云' });
 });
 
 test('inMemoryBtreeSearch - Not Found', t => {
   const res = query2.btreeSearch('10.10.10.10');
-  t.deepEqual(res, { city: 0, region: '内网IP|0|0|内网IP|内网IP' });
+  t.deepEqual(res, { city: 0, region: '0|0|0|内网IP|内网IP' });
 });
 
 test('inMemoryBtreeSearch Parse - Found', t => {
   const res = query2.btreeSearch('120.24.78.68', true);
-  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+  t.deepEqual(res, { id: 0, country: '中国', region: '0', province: '广东', city: '深圳', isp: '阿里云' });
 });
 
 test('inMemoryBtreeSearch Parse - Not Found', t => {
   const res = query2.btreeSearch('10.10.10.10', true);
-  t.deepEqual(res, { id: 0, country: '内网IP', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
+  t.deepEqual(res, { id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 });
 
 test('search - Found', t => {
   const res = query2.search('120.24.78.68');
-  t.deepEqual(res, { id: 2163, country: '中国', region: '华南', province: '广东省', city: '深圳市', isp: '阿里云' });
+  t.deepEqual(res, { id: 0, country: '中国', region: '0', province: '广东', city: '深圳', isp: '阿里云' });
 });
 
 test('search - Not Found', t => {
   const res = query2.search('10.10.10.10');
-  t.deepEqual(res, { id: 0, country: '内网IP', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
+  t.deepEqual(res, { id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 });
 
 test('search without Parse - Found', t => {
   const res = query2.search('120.24.78.68', false);
-  t.deepEqual(res, { city: 2163, region: '中国|华南|广东省|深圳市|阿里云' });
+  t.deepEqual(res, { city: 0, region: '中国|0|广东|深圳|阿里云' });
 });
 
 test('More Search Test', t => {
