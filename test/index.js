8093df891785af6d415332e86ed6a8741fe36c33
YourtionGuo <yourtion@gmail.com>
2018-06-15 17:48:45 +0800

    test: use jest

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 'use strict';
-
-import test from 'ava';
+/* eslint-env node, jest */
 
 const IP2Region = require('../lib');
 const query = new IP2Region({ inMemory: false });
@@ -11,117 +10,114 @@ const ALIYUN2 = Object.freeze({ id: 0, country: '中国', region: '0', province:
 const NEIWAN = Object.freeze({ city: 0, region: '0|0|0|内网IP|内网IP' });
 const NEIWAN2 = Object.freeze({ id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 
-test('btreeSearchSync - Found', t => {
+test('btreeSearchSync - Found', function () {
   const res = query.btreeSearch('120.24.78.68');
-  t.deepEqual(res, ALIYUN);
+  expect(res).toMatchObject(ALIYUN);
 });
 
-test('btreeSearchSync - Not Found', t => {
+test('btreeSearchSync - Not Found', function () {
   const res = query.btreeSearch('10.10.10.10');
-  t.deepEqual(res, NEIWAN);
+  expect(res).toMatchObject(NEIWAN);
 });
 
-test('btreeSearchSync Parse - Found', t => {
+test('btreeSearchSync Parse - Found', function () {
   const res = query.btreeSearch('120.24.78.68', true);
-  t.deepEqual(res, ALIYUN2);
+  expect(res).toMatchObject(ALIYUN2);
 });
 
-test('btreeSearchSync Parse - Not Found', t => {
+test('btreeSearchSync Parse - Not Found', function () {
   const res = query.btreeSearch('10.10.10.10', true);
-  t.deepEqual(res, NEIWAN2);
+  expect(res).toMatchObject(NEIWAN2);
 });
 
 
-test('binarySearchSync - Found', t => {
+test('binarySearchSync - Found', function () {
   const res = query.binarySearch('120.24.78.68');
-  t.deepEqual(res, ALIYUN);
+  expect(res).toMatchObject(ALIYUN);
 });
 
-test('binarySearchSync - Not Found', t => {
+test('binarySearchSync - Not Found', function () {
   const res = query.binarySearch('10.10.10.10');
-  t.deepEqual(res, NEIWAN);
+  expect(res).toMatchObject(NEIWAN);
 });
 
-test('binarySearchSync Parse - Found', t => {
+test('binarySearchSync Parse - Found', function () {
   const res = query.binarySearch('120.24.78.68', true);
-  t.deepEqual(res, ALIYUN2);
+  expect(res).toMatchObject(ALIYUN2);
 });
 
-test('binarySearchSync Parse - Not Found', t => {
+test('binarySearchSync Parse - Not Found', function () {
   const res = query.binarySearch('10.10.10.10', true);
-  t.deepEqual(res, NEIWAN2);
+  expect(res).toMatchObject(NEIWAN2);
 });
 
 
-test('inMemoryBinarySearch - Found', t => {
+test('inMemoryBinarySearch - Found', function () {
   const res = query2.binarySearch('120.24.78.68');
-  t.deepEqual(res, ALIYUN);
+  expect(res).toMatchObject(ALIYUN);
 });
 
-test('inMemoryBinarySearch - Not Found', t => {
+test('inMemoryBinarySearch - Not Found', function () {
   const res = query2.binarySearch('10.10.10.10');
-  t.deepEqual(res, NEIWAN);
+  expect(res).toMatchObject(NEIWAN);
 });
 
-test('inMemoryBinarySearch Parse - Found', t => {
+test('inMemoryBinarySearch Parse - Found', function () {
   const res = query2.binarySearch('120.24.78.68', true);
-  t.deepEqual(res, ALIYUN2);
+  expect(res).toMatchObject(ALIYUN2);
 });
 
-test('inMemoryBinarySearch Parse - Not Found', t => {
+test('inMemoryBinarySearch Parse - Not Found', function () {
   const res = query2.binarySearch('10.10.10.10', true);
-  t.deepEqual(res, NEIWAN2);
+  expect(res).toMatchObject(NEIWAN2);
 });
 
 
-test('inMemoryBtreeSearch - Found', t => {
+test('inMemoryBtreeSearch - Found', function () {
   const res = query2.btreeSearch('120.24.78.68');
-  t.deepEqual(res, ALIYUN);
+  expect(res).toMatchObject(ALIYUN);
 });
 
-test('inMemoryBtreeSearch - Not Found', t => {
+test('inMemoryBtreeSearch - Not Found', function () {
   const res = query2.btreeSearch('10.10.10.10');
-  t.deepEqual(res, NEIWAN);
+  expect(res).toMatchObject(NEIWAN);
 });
 
-test('inMemoryBtreeSearch Parse - Found', t => {
+test('inMemoryBtreeSearch Parse - Found', function () {
   const res = query2.btreeSearch('120.24.78.68', true);
-  t.deepEqual(res, ALIYUN2);
+  expect(res).toMatchObject(ALIYUN2);
 });
 
-test('inMemoryBtreeSearch Parse - Not Found', t => {
+test('inMemoryBtreeSearch Parse - Not Found', function () {
   const res = query2.btreeSearch('10.10.10.10', true);
-  t.deepEqual(res, NEIWAN2);
+  expect(res).toMatchObject(NEIWAN2);
 });
 
-test('search - Found', t => {
+test('search - Found', function () {
   const res = query2.search('120.24.78.68');
-  t.deepEqual(res, ALIYUN2);
+  expect(res).toMatchObject(ALIYUN2);
 });
 
-test('search - Not Found', t => {
+test('search - Not Found', function () {
   const res = query2.search('10.10.10.10');
-  t.deepEqual(res, NEIWAN2);
+  expect(res).toMatchObject(NEIWAN2);
 });
 
-test('search without Parse - Found', t => {
+test('search without Parse - Found', function () {
   const res = query2.search('120.24.78.68', false);
-  t.deepEqual(res, ALIYUN);
+  expect(res).toMatchObject(ALIYUN);
 });
 
-test('More Search Test', t => {
+test('More Search Test', function () {
   query2.search(-1);
   query2.search(0);
   query2.search(4294967040);
   query2.search('');
   query2.search('aa');
   query2.binarySearch('120.24.78.68');
-  t.pass();
 });
 
-test('Error - init with db file', t => {
-  const error = t.throws(() => {
-    new IP2Region({ dbPath: '/tmp/db.db' });
-  });
-  t.is(error.message, '[ip2region] db file not exists : /tmp/db.db');
+test('Error - init with db file', function () {
+  const error = () => new IP2Region({ dbPath: '/tmp/db.db' });
+  expect(error).toThrow('[ip2region] db file not exists : /tmp/db.db');
 });
