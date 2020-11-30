82b39c293ef2f32e2ec694b9fc1b7acc1916a6f0
YourtionGuo <yourtion@gmail.com>
2020-11-30 14:27:05 +0800

    feat: add isIPv4

--- test/index.js
+++ test/index.js
@@ -9,6 +9,7 @@ const ALIYUN_IP = '120.24.78.68';
 const ALIYUN = Object.freeze({ city: 2163, region: '中国|0|广东省|深圳市|阿里云' });
 const ALIYUN2 = Object.freeze({ id: 2163, country: '中国', region: '0', province: '广东省', city: '深圳市', isp: '阿里云' });
 const NEIWAN_IP = '10.10.10.10';
+const IPV6 = '2409:8946:2d51:1569:dfdb:6dcf:dd39:5d9a';
 const NEIWAN = Object.freeze({ city: 0, region: '0|0|0|内网IP|内网IP' });
 const NEIWAN2 = Object.freeze({ id: 0, country: '0', region: '0', province: '0', city: '内网IP', isp: '内网IP' });
 
@@ -21,6 +22,11 @@ describe('btreeSearchSync', function () {
   it('Not Found', function () {
     const res = queryInFS.btreeSearch(NEIWAN_IP);
     expect(res).toMatchObject(NEIWAN);
+  });
+
+  it('Not Found ipv6', function () {
+    const res = queryInFS.btreeSearch(IPV6);
+    expect(res).toBeNull();
   });
 
   it('Parse - Found', function () {
@@ -46,6 +52,11 @@ describe('binarySearchSync', function () {
     expect(res).toMatchObject(NEIWAN);
   });
 
+  it('Not Found ipv6', function () {
+    const res = queryInFS.binarySearch(IPV6);
+    expect(res).toBeNull();
+  });
+
   it('Parse - Found', function () {
     const res = queryInFS.binarySearch(ALIYUN_IP, true);
     expect(res).toMatchObject(ALIYUN2);
@@ -67,6 +78,11 @@ describe('inMemoryBinarySearch', function () {
   it('Not Found', function () {
     const res = queryInMemoey.binarySearch(NEIWAN_IP);
     expect(res).toMatchObject(NEIWAN);
+  });
+
+  it('Not Found ipv6', function () {
+    const res = queryInMemoey.binarySearch(IPV6);
+    expect(res).toBeNull();
   });
 
   it('Parse - Found', function () {
@@ -91,6 +107,11 @@ describe('inMemoryBtreeSearch', function () {
     expect(res).toMatchObject(NEIWAN);
   });
 
+  it('Not Found ipv6', function () {
+    const res = queryInMemoey.btreeSearch(IPV6);
+    expect(res).toBeNull();
+  });
+
   it('Parse - Found', function () {
     const res = queryInMemoey.btreeSearch(ALIYUN_IP, true);
     expect(res).toMatchObject(ALIYUN2);
@@ -113,6 +134,11 @@ describe('search', function () {
     expect(res).toMatchObject(NEIWAN2);
   });
 
+  it('Not Found ipv6', function () {
+    const res = queryInMemoey.search(IPV6);
+    expect(res).toBeNull();
+  });
+
   it('without Parse - Found', function () {
     const res = queryInMemoey.search(ALIYUN_IP, false);
     expect(res).toMatchObject(ALIYUN);
@@ -123,7 +149,8 @@ describe('More Tests', function () {
   it('Search Test', function () {
     queryInMemoey.search(-1);
     queryInMemoey.search(0);
-    queryInMemoey.search(4294967040);
+    queryInMemoey.search(1747920896);
+    queryInMemoey.search(3220758528);
     queryInMemoey.search('');
     queryInMemoey.search('aa');
     queryInMemoey.binarySearch(ALIYUN_IP);
